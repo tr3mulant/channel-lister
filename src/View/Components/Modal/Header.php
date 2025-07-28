665d84a2005f7d272a5336df541200b49433704b
Scott St. John <scottkstjohn87@gmail.com>
2025-07-28 09:07:26 -0700

    add a Modal component

---
+++ src/View/Components/Modal/Header.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IGE\ChannelLister\View\Components\Modal;
+
+class Header extends \Illuminate\View\Component
+{
+    public function render()
+    {
+        return view('channel-lister::components.modal.header');
+    }
+}
