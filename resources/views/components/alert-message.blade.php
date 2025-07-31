9bb31999a6ae56f0d2b35b417b7964872b9d590e
Scott St. John <scottkstjohn87@gmail.com>
2025-07-31 09:09:53 -0700

    fixed echoing out message with html in alert-message

--- resources/views/components/alert-message.blade.php
+++ resources/views/components/alert-message.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-{{ $alert_type }}" role="alert">
-    <strong>{{ $name }}</strong> {{ $message }}
-    <p>{{ $additional_text }}</p>
+    <strong>{{ $name }}</strong> {!! $message !!}
+    <p>{!! $additional_text !!}</p>
 </div>
